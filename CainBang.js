84300aa3269d6ad6e959773829217542e11b6b6f
DonGateley <DonGateley@gmail.com>
2016-06-09 22:55:50 -0700

    Update CainBang.js

--- CainBang.js
+++ CainBang.js
@@ -1,1 +1,10 @@
 //The creation point.  Provisional zero in Cain space.  Creating this file marks as much information as it can generate about the reference point in Cain Space called "CainBang".  Here goes...
+var sys = require("util"),
+my_http = require("http"),
+path = require("path"),
+url = require("url"),
+filesys = require("fs")
+;
+
+CainZero=0;
+CainCMB=0;
